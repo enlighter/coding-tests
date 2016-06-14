1592c93002f29af39ca93e3685600d7b46127b88
Sushovan Mandal <t-sumand@microsoft.com>
2016-06-14 18:30:07 +0530

    test normal string to url encoding in c#

---
+++ cSharp-UrlEncode/url-encode-test/url-encode-test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace url_encode_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://deblugger/Home/Search?q=" + Uri.EscapeDataString("berugram,burdwan,west bengal") + "+&la=22.57054&lo=88.37124&uln=" + Uri.EscapeDataString("Kolkata, India") + "&mf=en-in&cf=&rf=BLU&vf=&ec=" + Uri.EscapeDataString("BLU@enin:Default20100527_VS.QasDsX");
+            //url = Uri.EscapeDataString(url);
+
+            Console.WriteLine(url);
+        }
+    }
+}
